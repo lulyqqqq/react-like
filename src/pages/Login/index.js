e2f8a5c60062c11f15b3d98e915a7532e8e5c0bc
mfn <2976908894@qq.com>
2023-12-11 15:30:50 +0800

    登录页面-登录校验和数据收集

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,51 @@
+import React from 'react';
+import {Button, Card, Form, Input} from 'antd';
+import logo from '@/assets/logo.png'
+import './index.scss'
+
 const Login = () => {
+    const onFinish = (values) =>{
+        console.log(values)
+    }
     return (
-        <div>
-            this is Login
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={logo} alt="" />
+                {/* 登录表单 */}
+                <Form onFinish={onFinish} validateTrigger="onBlur">
+                    <Form.Item
+                        name="mobile"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Username!',
+                            },
+                            {
+                                pattern: /^1[3-9]\d{9}$/,
+                                message: '请输入正确的手机号！'
+                            }
+                        ]}>
+                        <Input size="large" placeholder="请输入账号" />
+                    </Form.Item>
+                    <Form.Item
+                        name="code"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}>
+                        <Input size="large" placeholder="请输入密码" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit" size="large" block>
+                            登录
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
         </div>
-    )
-}
-export default Login
+
+    );
+};
+export default Login;
