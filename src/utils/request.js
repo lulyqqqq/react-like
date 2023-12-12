2e516010621ecb716cec86660d448bebd8096aba
mfn <2976908894@qq.com>
2023-12-12 10:27:26 +0800

    token失效处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,8 @@
 // axios的封装处理
 
 import axios from 'axios'
-import {getToken} from "@/utils/token";
+import {getToken, removeToken} from "@/utils/token";
+import router from "@/router";
 
 const request = axios.create({
     baseURL: 'http://geek.itheima.net/v1_0',
@@ -29,6 +30,16 @@ request.interceptors.response.use((response) => {
 }, (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
+    // 监控401 token失效
+    if (error.response.status === 401) {
+        // 清除失效token
+        removeToken()
+        // 跳转登录
+        router.navigate('/login').then(r => {
+            // 强制刷新页面
+            window.location.reload()
+        })
+    }
     return Promise.reject(error)
 })
 
